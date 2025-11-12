8f67faaf96bfabcb67656a169a96eeb53f71958a
QueryVS <prgrmrart@gmail.com>
2025-11-12 08:51:57 +0300

    trivial update: select git branch when using script

--- scripts/install-all-server.sh
+++ scripts/install-all-server.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-git clone https://github.com/QueryVS/sysinfo-api/
+if [ "$1" == "prod" ]; then
+    git clone https://github.com/QueryVS/sysinfo-api/ -b main
+elif [ "$1" == "test" ]; then
+    git clone https://github.com/QueryVS/sysinfo-api/ -b develop/anc
+else
+    git clone https://github.com/QueryVS/sysinfo-api/ -b "$1"
+fi
+
 cd sysinfo-api/
 
 make
